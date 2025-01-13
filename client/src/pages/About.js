ec8fda027756c435337866c6b946787f9dc20262
suhail <suhail.khan@quintype.com>
2025-01-13 20:07:59 +0530

    about section

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,24 +1,23 @@
-("use client");
-
+import backgroundImage from "../assets/bg.jpg";
 export default function AboutUs() {
   return (
-    <div
-      id="about-section"
-      className="relative mb-10"
-      style={{
-        backgroundImage:
-          "url('https://cdn.create.vista.com/api/media/small/384446076/stock-photo-little-boy-sits-table-books-chooses-color-pencil-drawing-home')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        objectFit: "cover",
-        opacity: ".7",
-      }}
-    >
-      <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:py-32 text-center text-white">
-        <h1 className="text-3xl font-semibold tracking-tight sm:text-5xl">
+    <div id="about-section" className="relative mb-10">
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          objectFit: "cover",
+          opacity: 0.6,
+        }}
+      ></div>
+
+      <div className="relative mx-auto max-w-2xl py-16 sm:py-24 lg:py-32 text-center text-indigo-950">
+        <h1 className="text-2xl font-semibold tracking-tight sm:text-3xl">
           Empowering Minds, Shaping Futures
         </h1>
-        <p className="mt-8 text-white text-base font-large sm:text-base">
+        <p className="mt-8 text-sm font-semibold  sm:text-lg">
           Empowering young minds for a brighter future. At Apna School, we
           inspire holistic growth, foster creativity, and nurture potential
           through a world-class curriculum, state-of-the-art facilities, and
